129789a75680848e3457fa5bd1a6b6764bf1453d
EricSchles <Ericschles@gmail.com>
2015-04-24 12:05:10 -0400

    Update parser.py
    Parser now includes the beginnings of a parser.  Traversing still needs to be added.

--- parser.py
+++ parser.py
@@ -28,10 +28,12 @@ class HTMLNode:
 
 
 class Parser:
-    def __init__(self,document):
+    def __init__(self,document=''):
         self.document = document
         self.obj = self.transform(self.document)
 
+
+    #process html methods
     def transform(self,document):
         offset = document.find(">")
         if offset == -1:
@@ -40,33 +42,66 @@ class Parser:
             open_tag,close_tag,offset = self.find_tags(document)
             head = HTMLNode(open_tag,data=self.find_data(document),children=[])
             document = self.strip_doc(document,open_tag,close_tag)
-            return self._transform(document,head,head)
+            return self.dp_transform(document,head)
 
     def find_tags(self,document):
         offset = document.find(">")
         open_tag = document[:offset]+">"
-        close_tag = "</"+ open_tag.split("<")[1]
-        return open_tag,close_tag, offset
+        if "<" in open_tag: 
+            close_tag = "</"+ open_tag.split("<")[1]
+            return open_tag,close_tag,offset
+        else:
+            return "","",""
+
+    def cut(self,document,tag):
+        start = document.find(tag)
+        end = document[start:].find(">")+1
+        return document[:start]+document[start+end:]
+
 
     def find_data(self,document):
         offset = document.find(">")
         return document[offset:document[offset:].find("<")]
     
     def strip_doc(self,document,open_tag,close_tag):
-        return "<"+document.lstrip(open_tag).rstrip(close_tag)+">"
-        
-    def _transform(self,document,previous,head):
+        document = self.cut(document,open_tag)
+        document = self.cut(document,close_tag)
+        return document
+
+    #recursive - maybe more complete
+    def _transform(self,document,previous,head,count):
+        print count
         open_tag,close_tag,offset = self.find_tags(document)
         current = HTMLNode(open_tag,data=self.find_data(document),children=[],parent=previous)
         #sets reference so that child and parent nodes are linked
         previous.append_child(current)
         document = self.strip_doc(document,open_tag,close_tag)
-        if offset == -1:
+        if offset == -1 or document == '' or open_tag==None or close_tag==None:
             return head
         else:
-            return self._transform(document,current,head)
-        
+            return self._transform(document,current,head,count+1)
+    #iterative (dynamic programming) - possibly misses some cases, more testing required here
+    def dp_transform(self,document,head,offset=0,open_tag='',close_tag=''):
+        count = 0
+        previous = head
+        while offset != -1 or document != '' or open_tag!='' or close_tag!="":
+            #print offset
+            open_tag,close_tag,offset = self.find_tags(document)
+            if open_tag == '':
+                break
+            current = HTMLNode(open_tag,data=self.find_data(document),children=[],parent=previous)
+            #sets reference so that child and parent nodes are linked
+            previous.append_child(current)
+            document = self.strip_doc(document,open_tag,close_tag)
+            count += 1
+            previous = current
+        return head
 
 
     def __str__(self):
         return repr(self.obj)
+
+if __name__ == '__main__':
+    with open("test.html","r") as f:
+        obj = Parser(document=f.read())
+    print obj
